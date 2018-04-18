21d8121883c60ab226fd1d22ae4fc764dd3754b2
Luis Ángel Íñiguez Hernández <liniguez@wiggot.com>
2018-04-18 10:38:28 -0500

    first commit

---
+++ seed.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /home
+
+curl https://raw.githubusercontent.com/donluigimx-wiggot/mongo-seed/master/state.json > state.json
+curl https://raw.githubusercontent.com/donluigimx-wiggot/mongo-seed/master/city.json > city.json
+curl https://raw.githubusercontent.com/donluigimx-wiggot/mongo-seed/master/settlement.json > settlement.json
+
+mongoimport --host $DB_HOST --db wiggot --collection State --file /home/state.json
+mongoimport --host $DB_HOST --db wiggot --collection City --file /home/city.json
+mongoimport --host $DB_HOST --db wiggot --collection Settlement --file /home/settlement.json
