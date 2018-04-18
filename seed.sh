b2b67cc256452977647a002187c06a254d5cd784
Luis Ángel Íñiguez Hernández <liniguez@wiggot.com>
2018-04-18 11:06:38 -0500

    first commit

--- seed.sh
+++ seed.sh
@@ -6,6 +6,8 @@ curl https://raw.githubusercontent.com/donluigimx-wiggot/mongo-seed/master/state
 curl https://raw.githubusercontent.com/donluigimx-wiggot/mongo-seed/master/city.json > city.json
 curl https://raw.githubusercontent.com/donluigimx-wiggot/mongo-seed/master/settlement.json > settlement.json
 
+echo "Uploading seeds to $DB_HOST"
+
 mongoimport --host $DB_HOST --db wiggot --collection State --file /home/state.json
 mongoimport --host $DB_HOST --db wiggot --collection City --file /home/city.json
 mongoimport --host $DB_HOST --db wiggot --collection Settlement --file /home/settlement.json
